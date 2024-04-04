b6022d8277c6f5f6b004ea366087171eb2b468ed
Sachin Sagar <sachinssagar@gmail.com>
2024-04-04 17:49:35 +0530

    search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
@@ -15,21 +22,31 @@ const Header = () => {
               TrendifyMarket
             </Navbar.Brand>
           </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <FaShoppingCart /> Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FaUser /> Sign In
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
+          <Nav className="flex-grow-1 d-flex align-items-center justify-content-center">
+            <Form className="d-flex" role="search">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                className="me-2"
+              />
+              <Button variant="outline-light" type="submit">
+                Search
+              </Button>
+            </Form>
+          </Nav>
+          <Nav>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
         </Container>
       </Navbar>
     </header>
